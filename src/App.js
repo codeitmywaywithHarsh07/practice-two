6dca262f70abd50cc50181794691a8f3c16493e1
Harsh-winuall <harsh@winuall.com>
2024-08-06 10:54:04 +0530

    Releasing v1.0.2 Update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         <hr />
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur praesentium nihil eius libero, veritatis, reprehenderit delectus ab, aspernatur inventore eum officia quas impedit. Quibusdam repellat delectus accusantium tempora molestias. Veniam.
         Libero ad quia pariatur voluptates numquam facilis, eveniet quas harum earum voluptatem facere non ea odit accusamus distinctio hic amet blanditiis ut animi at adipisci similique cum. Itaque, consequatur! Adipisci?
-        Fugit obcaecati laudantium unde dolorum ratione facilis sed iste blanditiis magni, incidunt et corporis temporibus quam possimus quo recusandae voluptates adipisci soluta dolorem ipsam aperiam quos deserunt laborum assumenda. Ratione?
+        
         Velit, ullam! Dicta, libero laudantium. Eum voluptate optio numquam quos explicabo iste, ab nobis, vero delectus autem ex excepturi deserunt fugiat facere omnis, corporis laborum harum. Aspernatur nam vel ab.</p>
     </div>
   )
